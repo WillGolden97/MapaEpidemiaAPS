f462256ae83a7688b7bcfd667e74a26bfc225ca5
WillGolden97 <87283305+WillGolden97@users.noreply.github.com>
2021-07-11 20:29:48 -0300

    enviando

--- src/Connection/ConnectionFactory.java
+++ src/Connection/ConnectionFactory.java
@@ -21,7 +21,7 @@ import javax.swing.JOptionPane;
 public class ConnectionFactory {
     
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/aps";
+    private static final String URL = "jdbc:mysql://localhost:3306/viralcity";
     private static final String USER = "root";
     private static final String PASS = ""; 
     
